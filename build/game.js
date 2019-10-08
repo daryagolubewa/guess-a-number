a54d6692c1adbae1719a3a2124c030bb303fc09c
GolubevaDD <GolubevaDD@is.mos.ru>
2019-10-08 17:51:50 +0300

    listeners are separeted from the main file; necessary modules added;

--- build/game.js
+++ build/game.js
@@ -1,37 +1,11 @@
-function generateNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-;
-var randomNumber = generateNumber(0, 10);
-console.log('random', randomNumber);
-var elem = document.getElementById('result_message');
-document.getElementById('button')
-    .addEventListener('click', function () {
-    var inputNumberElem = document.getElementById('input_number');
-    // Если inputNumberElem не является экземлпяром класса HTMLInputElement, то показывать ошибку.
-    // Т.о., избавилась от приведения типов.
-    if (!(inputNumberElem instanceof HTMLInputElement)) {
-        throw new Error('An element with id input_number is not HTMLInputElement');
-    }
-    var getUserNumber = parseFloat(inputNumberElem.value);
-    document.getElementById('refresh_button').removeAttribute("disabled");
-    if (randomNumber === getUserNumber) {
-        elem.textContent = 'Hooray, you win!';
-    }
-    else if (randomNumber > getUserNumber) {
-        elem.textContent = 'Pick a greater number';
-    }
-    else if (randomNumber < getUserNumber) {
-        elem.textContent = 'Pick a smaller number';
-    }
-});
-document.getElementById('refresh_button')
-    .addEventListener('click', function () {
-    elem.textContent = '';
-    var inputNumberElem = document.getElementById('input_number');
-    if (!(inputNumberElem instanceof HTMLInputElement)) {
-        throw new Error('An element with id input_number is not HTMLInputElement');
-    }
-    inputNumberElem.value = '';
+define(["require", "exports", "./listeners"], function (require, exports, listeners_1) {
+    "use strict";
+    exports.__esModule = true;
+    exports.randomNumber = listeners_1.generateNumber(0, 10);
+    console.log('random', exports.randomNumber);
+    document.getElementById('button')
+        .addEventListener('click', listeners_1.onGuessButtonClick);
+    document.getElementById('refresh_button')
+        .addEventListener('click', listeners_1.onRefreshClick);
 });
 //# sourceMappingURL=game.js.map
--- game.ts
+++ game.ts
@@ -1,50 +1,14 @@
-function generateNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import {onGuessButtonClick, onRefreshClick, generateNumber} from "./listeners";
 
-let randomNumber: number = generateNumber(0, 10);
+export let randomNumber: number = generateNumber(0, 10);
 
 console.log('random', randomNumber);
 
-let elem = document.getElementById('result_message');
-
 document.getElementById('button')
-    .addEventListener('click', () => {
-
-        const inputNumberElem = document.getElementById('input_number');
-
-
-        // Если inputNumberElem не является экземлпяром класса HTMLInputElement, то показывать ошибку.
-        // Т.о., избавилась от приведения типов.
-        if (!(inputNumberElem instanceof HTMLInputElement)) {
-            throw new Error('An element with id input_number is not HTMLInputElement')
-        }
-
-
-        let getUserNumber: number = parseFloat(inputNumberElem.value);
-        document.getElementById('refresh_button').removeAttribute("disabled");
-
-        if (randomNumber === getUserNumber) {
-            elem.textContent = 'Hooray, you win!';
-        } else if (randomNumber > getUserNumber) {
-            elem.textContent = 'Pick a greater number'
-        } else if (randomNumber < getUserNumber) {
-            elem.textContent = 'Pick a smaller number'
-        }
-
-    });
+    .addEventListener('click', onGuessButtonClick);
 
 document.getElementById('refresh_button')
-    .addEventListener('click', () => {
-        elem.textContent = '';
-
-        const inputNumberElem = document.getElementById('input_number');
-        if (!(inputNumberElem instanceof HTMLInputElement)) {
-            throw new Error('An element with id input_number is not HTMLInputElement')
-        }
-
-        inputNumberElem.value = '';
-    });
+    .addEventListener('click', onRefreshClick);
 
 
 
